55429fb19c23b1f73361c8a8b63ae2264cf39adc
theverybusybee <v_antonchik@mail.ru>
2023-11-29 18:43:01 +0300

    arrayWithAttractionsForSearchInput added to data, search input fixed

--- src/components/page-sections/search-section/search-section.tsx
+++ src/components/page-sections/search-section/search-section.tsx
@@ -3,9 +3,10 @@
 import styles from "./search-section.module.scss";
 import SearchInput from "@/components/ui/inputs/search-input/search-input";
 import RoutePath from "@/components/ui/route-path/route-path";
+import { arrayWithAttractionsForSearchInput } from "@/constants/data-examples";
 import magnifyingGlassBold from "@/images/logos/icons/magnifyingGlass.svg";
 import clsx from "clsx";
-import { useMemo, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 
 interface Props {
   data: any;
@@ -15,57 +16,24 @@ interface Props {
 const SearchSection = ({ data, sectionSize = "full" }: Props) => {
   const bannerApiData = data?.Banner;
 
-  const input = useRef<any>();
-
-  const testData = [
-    "apple",
-    "apple2",
-    "apple3",
-    "banana split with apple split",
-    "carrot",
-    "date split split split apple",
-    "eggplant apple",
-    "fig",
-    "grape",
-    "split",
-  ];
-
-  const [searchState, setSearchState] = useState<any>(testData);
-  const [inputState, setInputState] = useState<string>();
+  const [searchState, setSearchState] = useState<string[]>([]);
+  const [inputState, setInputState] = useState<string>("");
 
   const containerClassName = clsx(styles.container, {
     [styles?.[`container_size_${sectionSize}`]]: sectionSize,
   });
 
-  // const setRequestedData = () => {
-  //   var xhr = new XMLHttpRequest();
-  //   // Отправка GET запроса к скрипту
-  //   xhr.open("GET", "/api/search?q=app", true);
-  //   xhr.onreadystatechange = function () {
-  //     if (xhr.readyState === 4 && xhr.status === 200) {
-  //       // Обработка полученного ответа
-  //       //  var response = JSON.parse(xhr.responseText);
-  //       console.log(xhr); // Вывод результатов в консоль
-  //     }
-  //   };
-  //   xhr.send();
-  // };
-
-  // useEffect(() => {
-  //   setRequestedData();
-  // }, []);
-
   const onInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setInputState(e?.target.value);
   };
 
-  const testArr = useMemo(() => {
-    return (
-      searchState.length &&
-      inputState &&
-      searchState.filter((el: string) => el.includes(inputState))
+  useEffect(() => {
+    setSearchState(
+      arrayWithAttractionsForSearchInput.filter((el) =>
+        el.toLowerCase().includes(inputState.toLocaleLowerCase())
+      )
     );
-  }, [inputState]);
+  }, [inputState.length]);
 
   return bannerApiData ? (
     <section
@@ -82,7 +50,7 @@ const SearchSection = ({ data, sectionSize = "full" }: Props) => {
             extraClass={styles.input}
             Icon={magnifyingGlassBold}
             description={bannerApiData.search.text}
-            refProp={input}
+            // refProp={input}
             onChange={onInputChange}
           />
           <div
@@ -91,50 +59,17 @@ const SearchSection = ({ data, sectionSize = "full" }: Props) => {
             }`}
           >
             <ul className={styles.searchDropDownList}>
-              {testArr?.length &&
-                testArr.map((el: string, index: number) => {
-                  const arr = el.split(" ");
+              {searchState?.length !== 0 ? (
+                searchState.map((el: string, index: number) => {
                   return (
                     <li className={styles.searchDropDownItem} key={index}>
-                      {arr.map((word: string, index: number) => {
-                        return (
-                          <span
-                            className={styles.searchDropDownItem}
-                            key={index}
-                          >
-                            {!(inputState && word.includes(inputState)) ? (
-                              <>{word} </>
-                            ) : (
-                              <>
-                                {word.split("").map((letter, index: number) => {
-                                  if (
-                                    inputState &&
-                                    letter === inputState[index]
-                                  ) {
-                                    return (
-                                      <span
-                                        className={styles.span_type_bold}
-                                        key={index}
-                                      >
-                                        {word?.split("")?.length - 1 ===
-                                        index ? (
-                                          <>{letter} </>
-                                        ) : (
-                                          <>{letter}</>
-                                        )}
-                                      </span>
-                                    );
-                                  }
-                                  return letter;
-                                })}
-                              </>
-                            )}
-                          </span>
-                        );
-                      })}
+                      {el}
                     </li>
                   );
-                })}
+                })
+              ) : (
+                <li className={styles.searchDropDownItem}>{"Not Found"}</li>
+              )}
             </ul>
           </div>
         </div>
