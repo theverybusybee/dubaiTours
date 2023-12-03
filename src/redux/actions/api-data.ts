ea9ad0d7f1e7f7a2e9d0019d9bb5accd2bf2bec0
theverybusybee <v_antonchik@mail.ru>
2023-12-04 02:15:02 +0300

    unnecessary code removed

--- src/redux/actions/api-data.ts
+++ src/redux/actions/api-data.ts
@@ -1,6 +1,5 @@
 import {
   getReservationDataByTourId,
-  getReservationUpdatedData,
 } from "@/utils/fetches";
 import {
   GET_RESERVATION_DATA_FAILED,
@@ -8,7 +7,6 @@ import {
   GET_RESERVATION_DATA_SUCCESS,
 } from "../constants/reservation-modal";
 import { TDispatch } from "../store";
-import { IReservationFormData } from "@/utils/types";
 
 export function getReservationModalData(id: string) {
   return function (dispatch: TDispatch) {
@@ -31,25 +29,3 @@ export function getReservationModalData(id: string) {
       );
   };
 }
-
-// export function getReservationReceiptData(form: IReservationFormData) {
-//   return function (dispatch: TDispatch) {
-//     dispatch({ type: GET_RESERVATION_DATA_REQUEST });
-//     getReservationUpdatedData(form)
-//       .then((res) => {
-//         if (res.Reserve) {
-//           dispatch({
-//             type: GET_RESERVATION_DATA_SUCCESS,
-//             payload: res.Reserve,
-//           });
-//         } else {
-//           dispatch({ type: GET_RESERVATION_DATA_FAILED });
-//         }
-//       })
-//       .catch(() =>
-//         dispatch({
-//           type: GET_RESERVATION_DATA_FAILED,
-//         })
-//       );
-//   };
-// }
